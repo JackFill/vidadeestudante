7e3b58bc68a648649071d43772b080c5b10a31e8
JackFill <JackFill@users.noreply.github.com>
2014-04-18 17:12:10 -0300

    Update models.py

--- blog/autor/models.py
+++ blog/autor/models.py
@@ -1,8 +1,6 @@
 #-*-coding:utf-8 -*-
 from django.db import models
 
-'''
-file:///C:/Downloads/django/django-docs-dev-en/ref/class-based-views/generic-editing.html#django.views.generic.edit.FormView
-'''
+
 #todo autor tem um artigo ou mais
 #todo autor tem um perfil
