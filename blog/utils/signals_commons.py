0178f433a6a04a05e04731c5afa874253270adbe
JackFill <JackFill@users.noreply.github.com>
2014-04-18 17:10:34 -0300

    Update signals_commons.py

--- blog/utils/signals_commons.py
+++ blog/utils/signals_commons.py
@@ -1,20 +1,3 @@
 #-*-coding:utf-8 -*-
-from django.template.defaultfilters import Slugify
 
-"""Este signal gera um slug automaticamente. Ele verifica se ja existe um
-    objeto com o mesmo slug e acrescenta um numero ao final para evitar
-    duplicidade"""
-
-
-def slug_pre_save(signal, instance, sender, **kwargs):
-	if not instance.slug:
-		slug = slugfy(instance.titulo)
-		new_slug = slug
-		counter = 0
-		
-		
-		while sender.objects.filter(slug=new_slug).exclude(id=instance.id).count() > 0:
-			counter +=1
-			new_slug = '%s - %d' %(slug, counter)
-		
-		instance.slug = new_slug
+#add tools utils
