9569242aa34f4462f1a03119a3703430d93b99b5
JackFill <JackFill@users.noreply.github.com>
2014-04-19 16:33:25 -0300

    Delete forms.py

--- blog/user_profile/forms.py
+++
@@ -1,9 +0,0 @@
-#-*-coding:utf-8 -*-
-
-from django import forms
-
-
-class AutorForm(form.Form):
-	image = forms.ImageField()
-	profissao = models.CharField(max_length=2,widget=forms.select(choices=PF_CHOICES))
-	
